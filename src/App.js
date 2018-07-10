aef916e22d3db7b6ece8309de0a73192f5d69389
dhoonjang <dhoonjang@gmail.com>
2018-07-10 15:52:12 +0900

    Catch Error 연습

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import {createPortal} from 'react-dom';
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["gyuhwan", "jungho", "junsu", "myunggyu"]
+  };
+
+  componentDidMount = () => {
+    setTimeout(()=>{
+      this.setState({
+        friends: undefined
+      })
+    }, 5000)
+  }
+
+  render() {
+    const { friends } = this.state;
+
+    return friends.map((friend,index) => <Friend name={friend} key={index}/>);
+  }
+}
+
+const Friend = ({name}) => {
+  return ' ' + name + ' ';
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchme"));
@@ -13,16 +37,30 @@ class ReturnTypes extends Component {
   }
 }
 
-const Message = () => {
-  return "Touch!";
-}
+const Message = () => 'Touch!!!'
+
+const ErrorFallBack = () => ' sorry something went wrong ㅠㅠ'
 
 class App extends Component {
+  state = {
+    errorExist: false
+  }
+
+  componentDidCatch = (error, info) => {
+    this.setState({
+      errorExist: true
+    })
+
+    console.log('catched '+ error + ' the info I have is ' + info )
+  }
+
   render() {
+    const {errorExist} = this.state;
     return (
       <Fragment>
         <ReturnTypes />
         <Portals />
+        {errorExist ? <ErrorFallBack />:<ErrorMaker />}
       </Fragment>
     );
   }
